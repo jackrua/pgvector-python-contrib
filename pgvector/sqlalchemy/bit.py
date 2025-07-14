ac8a9c98b4fcc0469ca4d8773fe4be5df9623bc5
Giacomo rua <giacomo.rua@outlook.com>
2025-07-14 22:15:06 +0200

    almost there

--- pgvector/sqlalchemy/bit.py
+++ pgvector/sqlalchemy/bit.py
@@ -1,6 +1,7 @@
+from sqlalchemy.dialects.postgresql.asyncpg import PGDialect_asyncpg
 from sqlalchemy.dialects.postgresql.base import ischema_names
 from sqlalchemy.types import UserDefinedType, Float
-
+from .. import Bit
 
 class BIT(UserDefinedType):
     cache_ok = True
@@ -13,6 +14,16 @@ class BIT(UserDefinedType):
         if self.length is None:
             return 'BIT'
         return 'BIT(%d)' % self.length
+
+    def bind_processor(self, dialect):
+        def process(value):
+            if isinstance(dialect, PGDialect_asyncpg): 
+                if value is None: 
+                    return None
+                return (ord(value._data) >> (len(value._data) * 8) - value._len).to_bytes()
+            return Bit._to_db(value)
+        return process
+
 
     class comparator_factory(UserDefinedType.Comparator):
         def hamming_distance(self, other):
