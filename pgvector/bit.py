44705d7228fe1e467b55d618a6c0d01df0d870b9
Giacomo rua <giacomo.rua@outlook.com>
2025-07-17 17:35:02 +0200

    feat: add method to read from database and make writing to database method more general

--- pgvector/bit.py
+++ pgvector/bit.py
@@ -73,3 +73,9 @@ class Bit:
             raise ValueError('expected bit')
 
         return value.to_binary()
+    
+    @classmethod
+    def _from_db(cls, value):
+        if value is None or isinstance(value, cls): 
+            return value
+        return cls.from_text(value)
